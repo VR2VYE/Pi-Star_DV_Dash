b1bf79ea2515d9a1a6d68a0b1fd27ea2e251266e
VR2VYE <vr2vye@gmail.com>
2018-07-06 21:27:32 +0800

    update

--- admin/configure.php
+++ admin/configure.php
@@ -687,7 +687,9 @@ if ($_SERVER["PHP_SELF"] == "/admin/configure.php") {
 	// Set Callsign
 	if (empty($_POST['confCallsign']) != TRUE ) {
 	  $newCallsignUpper = strtoupper(preg_replace('/[^A-Za-z0-9]/', '', $_POST['confCallsign']));
-	  if (preg_match("/^[0-9]/", $newCallsignUpper)) { $newCallsignUpperIRC = 'r'.$newCallsignUpper; } else { $newCallsignUpperIRC = $newCallsignUpper; }
+	  // Removed the need for the r prefix - OpenQuad have fixed up the servers not to require it.
+	  // if (preg_match("/^[0-9]/", $newCallsignUpper)) { $newCallsignUpperIRC = 'r'.$newCallsignUpper; } else { $newCallsignUpperIRC = $newCallsignUpper; }
+	  $newCallsignUpperIRC = $newCallsignUpper;
 
 	  $rollGATECALL = 'sudo sed -i "/gatewayCallsign=/c\\gatewayCallsign='.$newCallsignUpper.'" /etc/ircddbgateway';
 	  $rollDPLUSLOGIN = 'sudo sed -i "/dplusLogin=/c\\dplusLogin='.$newCallsignUpper.'" /etc/ircddbgateway';
@@ -1510,7 +1512,7 @@ if ($_SERVER["PHP_SELF"] == "/admin/configure.php") {
 	  system($rollMotd);
 	  if (file_exists('/etc/hostapd/hostapd.conf')) {
 		  // Update the Hotspot name to the Hostname
-		  $rollApSsid = 'sudo sed -i "/ssid=/c\\ssid='.$newHostnameLower.'" /etc/hostapd/hostapd.conf';
+		  $rollApSsid = 'sudo sed -i "/^ssid=/c\\ssid='.$newHostnameLower.'" /etc/hostapd/hostapd.conf';
 		  system($rollApSsid);
 	  }
 	}
