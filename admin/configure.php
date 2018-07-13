a6636a1032074b4cff1c25313b7e2d3f17a75e39
VR2VYE <35602032+VR2VYE@users.noreply.github.com>
2018-07-13 23:08:55 +0800

    Update configure.php

--- admin/configure.php
+++ admin/configure.php
@@ -214,8 +214,7 @@ $MYCALL=strtoupper($callsign);
  <a href="/admin/expert/" style="color: #ffffff;">Expert</a> |
  <a href="/admin/power.php" style="color: #ffffff;"><?php echo $lang['power'];?></a> |
  <a href="/admin/update.php" style="color: #ffffff;"><?php echo $lang['update'];?></a> |
- <a href="/admin/config_backup.php" style="color: #ffffff;"><?php echo $lang['backup_restore'];?></a> |
- <a href="/admin/nextion.php" style="color: #ffffff;"><?php echo $lang['nextion'];?></a>	
+ <a href="/admin/config_backup.php" style="color: #ffffff;"><?php echo $lang['backup_restore'];?></a> |	
  <a href="javascript:factoryReset();" style="color: #ffffff;"><?php echo $lang['factory_reset'];?></a>
 </p>
 </div>
